56da0d9c6fcf5d6bfac8a64e2d25aad150bf4903
Arthaeris <the.shou7@googlemail.com>
2026-08-08 11:41:41 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const WORLD = {
+  width: 512,
+  height: 512,
+  tileSize: 16
+};
+
+export const AREAS = {
+  outerForest: {
+    x: 0,
+    y: 0,
+    width: 512,
+    height: 80
+  },
+
+  futureFacilities: {
+    x: 64,
+    y: 70,
+    width: 384,
+    height: 60
+  },
+
+  park: {
+    x: 56,
+    y: 120,
+    width: 400,
+    height: 235
+  },
+
+  starterArea: {
+    x: 206,
+    y: 285,
+    width: 100,
+    height: 55
+  },
+
+  mainStreet: {
+    x: 0,
+    y: 400,
+    width: 512,
+    height: 28
+  },
+
+  residential: {
+    x: 0,
+    y: 428,
+    width: 512,
+    height: 84
+  }
+};
